93e929b58bcd04f74fd1bd6c7c9ef5e41ef9c210
counsellormalone <counsellormalone@gmail.com>
2024-06-30 17:35:31 +1000

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ const testimonials = [
   {
     name: "Dave",
     job: "Past client",
-    image: "https://i.postimg.cc/mgp4pfz5/profile-image-1.png",
     testimonial:
       "You (Chris) are one of the only people I've liked coming to. Everyone else lectures me about how the brain works. I already know all that. You actually listen to me and help me with things that are happening in my life.",
   },
   {
     name: "Stacey",
     job: "Mother of past client",
-    image: "https://i.postimg.cc/PfSSwtB9/profile-image-2.png",
     testimonial:
       "We have seen such a change in Shawn since he started coming to see you. His confidence is so much higher and his (issue) that he came to see you about is no longer a problem for him. I would love for him to continue working with you if possible, or come back to see you in the future",
   },
@@ -37,7 +35,6 @@ prevBtn.addEventListener("click", () => {
 let displayTestimonial = () => {
   testimonialContainer.innerHTML = `
     <p>${testimonials[i].testimonial}</p>
-    <img src=${testimonials[i].image}>
     <h3>${testimonials[i].name}</h3>
     <h6>${testimonials[i].job}</h6>
   `;
